d2d8228d0837ba91cf61878812b7f571b4a815bc
Manas Bajaj <manas.bajaj94@gmail.com>
2025-03-11 09:18:46 +0000

    Revert "grid view for problems page"
    This reverts commit 9c484d11f152a7fe5f882809a81552f5f8d06e6c.

--- src/pages/problems-list-page.tsx
+++ src/pages/problems-list-page.tsx
@@ -1,92 +1,59 @@
 import CodeIcon from "@mui/icons-material/Code";
 import DoneAllIcon from "@mui/icons-material/DoneAll";
-import ShuffleIcon from "@mui/icons-material/Shuffle";
 import {
   Alert,
-  Button,
   CircularProgress,
-  Container,
-  Link,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   Typography,
 } from "@mui/material";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { useCallback, useMemo } from "react";
-import { Link as RouterLink, useNavigate, useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import { useAuthContext } from "../auth/auth-context";
 import { DifficultyChip } from "../components/difficulty-chip";
 import { TagChip } from "../components/tag-chip";
 import { useGetProblems } from "../store/problems";
 import { IProblem, IProblemCategory } from "../types";
 
+const Problem = ({
+  problem,
+  isCompleted,
+}: {
+  problem: IProblem;
+  isCompleted: boolean;
+}) => {
+  return (
+    <ListItem classes={{ root: "!py-0 !px-2" }}>
+      <ListItemIcon classes={{ root: "!min-w-[40px]" }}>
+        {isCompleted ? (
+          <DoneAllIcon color="success" fontSize="small" />
+        ) : (
+          <CodeIcon fontSize="small" />
+        )}
+      </ListItemIcon>
+      <ListItemText>
+        <div className="flex gap-2">
+          <Link to={problem.id}>
+            <Typography variant="body1">{problem.title}</Typography>
+          </Link>
+          {problem.tags.map((tag) => (
+            <TagChip key={tag} tag={tag} />
+          ))}
+          <DifficultyChip difficulty={problem.difficulty} />
+        </div>
+      </ListItemText>
+    </ListItem>
+  );
+};
+
 export const ProblemsListPage = () => {
   const { category } = useParams<{ category: IProblemCategory }>();
   const { user } = useAuthContext();
 
-  const columns: GridColDef<IProblem>[] = useMemo(() => {
-    return [
-      {
-        field: "status",
-        headerName: "Status",
-        cellClassName: "flex items-center",
-        valueGetter: (_, row) => user.completions[row.id],
-        renderCell: (params) => {
-          if (params.value) {
-            return <DoneAllIcon color="success" fontSize="small" />;
-          } else {
-            return <CodeIcon fontSize="small" />;
-          }
-        },
-      },
-      {
-        field: "title",
-        headerName: "Title",
-        flex: 1,
-        cellClassName: "flex items-center",
-        renderCell: (params) => {
-          return (
-            <Link underline="hover" component={"div"}>
-              <RouterLink to={params.row.id}>
-                <Typography
-                  className="cursor-pointer"
-                  variant="body1"
-                  fontSize={16}
-                  fontWeight={400}
-                >
-                  {params.value}
-                </Typography>
-              </RouterLink>
-            </Link>
-          );
-        },
-      },
-      {
-        field: "difficulty",
-        headerName: "Difficulty",
-        renderCell: (params) => <DifficultyChip difficulty={params.value} />,
-      },
-      {
-        field: "tags",
-        headerName: "Tags",
-        valueGetter: (_, row) => row.tags.join(","),
-        renderCell: (params) => {
-          return params.row.tags.map((tag) => <TagChip key={tag} tag={tag} />);
-        },
-      },
-    ];
-  }, [user.completions]);
-
   if (!category) throw new Error("Category cannot be undefined!");
 
   const problems = useGetProblems(category);
-
-  const navigate = useNavigate();
-
-  const handleRandomClick = useCallback(() => {
-    if (!problems.data) return;
-    const randomProblem =
-      problems.data[Math.floor(Math.random() * problems.data.length)];
-    navigate(`/problems/${category}/${randomProblem.id}`);
-  }, [problems.data, category, navigate]);
 
   if (problems.isPending) {
     return <CircularProgress size={"small"} />;
@@ -105,48 +72,19 @@ export const ProblemsListPage = () => {
   }
 
   return (
-    <Container maxWidth="lg" className="!ml-0">
-      <div className="flex flex-row justify-between items-center">
-        <Typography variant={"h5"}>{category} Problems</Typography>
-        <div className="flex items-center gap-2">
-          <Typography color="textSecondary" variant={"body2"}>
-            {Object.keys(user.completions).length} / {problems.data.length}{" "}
-            completed
-          </Typography>
-          <Button
-            endIcon={<ShuffleIcon fontSize="small" />}
-            size="small"
-            variant="outlined"
-            loading={problems.isPending}
-            onClick={handleRandomClick}
-          >
-            Random
-          </Button>
-        </div>
-      </div>
-      <div className="flex flex-column mt-3">
-        <DataGrid
-          initialState={{
-            sorting: { sortModel: [{ field: "title", sort: "asc" }] },
-          }}
-          density="compact"
-          rows={problems.data}
-          columns={columns}
-          disableRowSelectionOnClick
-          disableMultipleRowSelection
-          hideFooter
-          disableColumnSelector
-          rowSelection={false}
-          sx={{
-            "& .MuiDataGrid-cell:focus, & .MuiDataGrid-cell:focus-within": {
-              outline: "none",
-            },
-            "& .MuiDataGrid-row:hover": {
-              backgroundColor: "inherit", // Or 'transparent' or whatever color you'd like
-            },
-          }}
-        />
-      </div>
-    </Container>
+    <div>
+      <Typography variant={"h5"}>
+        {category} Problems ({problems.data.length})
+      </Typography>
+      <List>
+        {problems.data?.map((problem) => (
+          <Problem
+            problem={problem}
+            key={problem.id}
+            isCompleted={user.completions[problem.id] === true}
+          />
+        ))}
+      </List>
+    </div>
   );
 };
